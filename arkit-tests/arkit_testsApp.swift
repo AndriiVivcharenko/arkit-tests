b95bb6d3242f433db94f30bb7cc89723ac388286
AndriiVivcharenko <andrii.v@radency.com>
2024-02-26 14:33:33 +0200

    Initial Commit

---
+++ arkit-tests/arkit_testsApp.swift
@@ -0,0 +1,17 @@
+//
+//  arkit_testsApp.swift
+//  arkit-tests
+//
+//  Created by Andrii Vivcharenko on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct arkit_testsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
